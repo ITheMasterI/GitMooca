48dd6221399acc9f7098a44ceb51cbc869183b21
ITheMasterI <pedro.carmona02@hotmail.com>
2021-03-08 21:14:35 -0300

    Primeiro teste concluido com sucesso

--- Empregado.java
+++ Empregado.java
@@ -21,6 +21,25 @@ this.tipo = tipo;
 }
 
 
+public void setSalario (double salario){
+    this.salario = salario;
+    }
+    public double getSalario (){
+    return this.salario;
+    }
+    public void setComissao (double comissao){
+    this.comissao = comissao;
+    }
+    public double getComissao (){
+    return this.comissao;
+    }
+    public void setBonus (double bonus){
+    this.bonus = bonus;
+    }
+    public double getBonus (){
+    return this.bonus;
+    }
+
     public double calculaSalario (){
     if (tipo == 1){
     return salario;
